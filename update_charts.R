bcf417e74dc18756145c0162fbf0668250d55acb
John Kelly <jkelly3rd@gmail.com>
2025-01-31 10:34:15 -0500

    Start

---
+++ update_charts.R
@@ -0,0 +1,18 @@
+# Load the datawrapper package
+library(DatawRappr)
+library(rdwd)
+
+# Retrieve API key from env
+api_key <- Sys.getenv("DATAWRAPPER_API_KEY")
+
+# Check if API key is present
+if (api_key == "") {
+  stop("Datawrapper API key not found. Please set the DATAWRAPPER_API_KEY environment variable.")
+}
+
+# Authenticate with Datawrapper
+datawrapper_auth(api_key)
+
+# Publish the charts
+dw_publish_chart(chart_id = 'l6Ftz')
+dw_publish_chart(chart_id = 'NFqzJ')
