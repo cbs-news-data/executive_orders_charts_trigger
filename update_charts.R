37fce1014c395488873cedc6aa8b839fc1b72f4c
John Kelly <jkelly3rd@gmail.com>
2025-01-31 11:16:56 -0500

    A

--- update_charts.R
+++ update_charts.R
@@ -1,3 +1,11 @@
+# Install the necessary packages if not already installed
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools")
+}
+if (!requireNamespace("DatawRappr", quietly = TRUE)) {
+  devtools::install_github("munichrocker/DatawRappr")
+}
+
 # Load the datawrapper package
 library(DatawRappr)
 library(rdwd)
