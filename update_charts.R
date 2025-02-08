416b8e4261f0ba5e71c011c2cba49d029b69612c
John Kelly <jkelly3rd@gmail.com>
2025-02-08 14:43:41 -0500

    Aa

--- update_charts.R
+++ update_charts.R
@@ -1,4 +1,5 @@
 library(DatawRappr)
+library(lubridate)
 
 # Retrieve API key from env
 api_key <- Sys.getenv("DATAWRAPPER_API_KEY")
@@ -11,7 +12,11 @@ if (api_key == "") {
 # Authenticate with Datawrapper
 datawrapper_auth(api_key)
 
-dw_edit_chart(chart_id = 'NFqzJ', intro = 'Updated: Feb 8 at 1 p.m.<br><br>
+formatted_time <- format(with_tz(Sys.time(), tzone = "America/New_York"), "%b. %d at %I:%M %p")
+# Remove the leading zero from the day
+formatted_time <- gsub(" 0", " ", formatted_time)
+
+dw_edit_chart(chart_id = 'NFqzJ', intro = paste0('<b>As of ',formatted_time,'</b>.
               The latest executive orders issued by President Trump, plus the process ahead and legal issues. This only includes orders and not actions such as memos and pardons. You can click the buttons to see certain categories or search them all: <br><br>
 
 <span style="line-height:30px">
@@ -38,8 +43,8 @@ dw_edit_chart(chart_id = 'NFqzJ', intro = 'Updated: Feb 8 at 1 p.m.<br><br>
               
               <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=Election" style="background:navy; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Election </a> &nbsp;
               
-              </span>')
+              </span>'))
 
 # Publish the charts
-#dw_publish_chart(chart_id = 'l6Ftz')
+dw_publish_chart(chart_id = 'l6Ftz')
 dw_publish_chart(chart_id = 'NFqzJ')
