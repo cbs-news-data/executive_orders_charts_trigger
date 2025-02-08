b5798e11e9e4aa823c314553f560f69b5edf501d
John Kelly <jkelly3rd@gmail.com>
2025-02-08 13:41:56 -0500

    aa

--- update_charts.R
+++ update_charts.R
@@ -11,6 +11,35 @@ if (api_key == "") {
 # Authenticate with Datawrapper
 datawrapper_auth(api_key)
 
+dw_edit_chart(chart_id = 'NFqzJ', intro = 'Updated: Feb 8 at 1 p.m.<br><br>
+              The latest executive orders issued by President Trump, plus the process ahead and legal issues. This only includes orders and not actions such as memos and pardons. You can click the buttons to see certain categories or search them all: <br><br>
+
+<span style="line-height:30px">
+
+<a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/" style="background:purple; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> All</a>&nbsp;
+                
+                <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=Environment" style="background:#429ddd; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Environment </a> &nbsp;
+              
+              <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=Immigration" style="background:gray; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Immigration </a> &nbsp;
+              
+              <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=Workforce" style="background:maroon; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Federal Workers </a> &nbsp;
+              
+              <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=Energy" style="background:red; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Energy </a> &nbsp;
+              
+              <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=technology" style="background:black; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Technology </a> &nbsp;
+              
+              <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=health" style="background:green; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Health </a> &nbsp;
+              
+              <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=foreign" style="background:blue; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Foreign Policy </a> &nbsp;
+              
+              <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=Defense" style="background:black; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Defense </a> &nbsp;
+              
+              <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=Diversity" style="background:gold; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Diversity </a> &nbsp;
+              
+              <a target="_self" href="https://datawrapper.dwcdn.net/NFqzJ/?search=Election" style="background:navy; padding:1px 6px; border-radius:5px; color:#ffffff; font-weight:400; box-shadow:0px 0px 7px 2px rgba(0,0,0,0.07); cursor:pointer;" rel="nofollow noopener"> Election </a> &nbsp;
+              
+              </span>')
+
 # Publish the charts
-dw_publish_chart(chart_id = 'l6Ftz')
+#dw_publish_chart(chart_id = 'l6Ftz')
 dw_publish_chart(chart_id = 'NFqzJ')
