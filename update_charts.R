a6518edfcb433e7c7296326e8e874986552aa431
John Kelly <jkelly3rd@gmail.com>
2025-02-12 09:12:02 -0500

    A

--- update_charts.R
+++ update_charts.R
@@ -3,12 +3,10 @@ library(lubridate)
 
 # Retrieve API key from env
 api_key <- Sys.getenv("DATAWRAPPER_API_KEY")
-
 # Check if API key is present
 if (api_key == "") {
   stop("Datawrapper API key not found. Please set the DATAWRAPPER_API_KEY environment variable.")
 }
-
 # Authenticate with Datawrapper
 datawrapper_auth(api_key)
 
